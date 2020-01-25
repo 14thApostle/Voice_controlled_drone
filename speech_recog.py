2b56bfeb91586780ba407e50064524f4f02fa6c4
14thApostle <irfaan365@gmail.com>
2020-01-25 06:45:45 +0530

    Initial commit

---
+++ speech_recog.py
@@ -0,0 +1,24 @@
+import speech_recognition as sr
+print(sr.__version__)
+
+
+
+
+def return_speech():
+    r = sr.Recognizer()
+    mic = sr.Microphone()
+
+    with mic as source:
+        print("Speak up")
+        r.adjust_for_ambient_noise(source)
+        audio = r.listen(source)
+
+    print("recognizing")
+    data = r.recognize_google(audio)
+    print(data)
+    return data
+
+for i in range(3):
+    print("{}th Try".format(i))
+    print("You said {}\n\n".format(return_speech()))
+
