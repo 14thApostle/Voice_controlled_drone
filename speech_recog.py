638e149c369893d917e68b2fcfc20b91d5c7edfb
DarkcrusherX <ahalmartinv@gmail.com>
2020-01-26 22:12:50 +0530

    1 commit

--- speech_recog.py
+++ speech_recog.py
@@ -1,8 +1,8 @@
 import speech_recognition as sr
+import rospy
+from std_msgs.msg import String
+
 print(sr.__version__)
-
-
-
 
 def return_speech():
     r = sr.Recognizer()
@@ -18,7 +18,18 @@ def return_speech():
     print(data)
     return data
 
-for i in range(3):
-    print("{}th Try".format(i))
-    print("You said {}\n\n".format(return_speech()))
+#while True:
+def talker():
+    pub = rospy.Publisher('voice_cammands', String, queue_size=10)
+    rospy.init_node('talker', anonymous=True)
+    rate = rospy.Rate(10)
+    while not rospy.is_shutdown():
 
+        print("You said {}\n\n".format(return_speech()))
+        pub.publish(return_speech())
+        rate.sleep()
+if __name__ == '__main__':
+    try:
+        talker()
+    except:
+        pass
