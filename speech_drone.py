65b3d54262b7797f701fef82435107e0c71b7960
DarkcrusherX <ahalmartinv@gmail.com>
2020-01-27 13:20:46 +0530

    3 commit

--- speech_drone.py
+++ speech_drone.py
@@ -5,14 +5,16 @@ from mavros_msgs.srv import CommandBool
 from mavros_msgs.srv import CommandTOL
 from mavros_msgs.srv import SetMode
 from geometry_msgs.msg import PoseStamped 
+from mavros import command
 data =  ""
+
 
 rospy.init_node('todrone', anonymous=True)
 
 def callback(data1):
     global data
     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data1.data)
-    data = data1.data
+    data = str(data1.data)
     print(data)
     assign(data)
 
@@ -21,23 +23,26 @@ rospy.Subscriber("/voice", String, callback)
 
 
 def assign(data):
-    data.split(" ")
-    print(data)
+    data = data.split(" ")
+    print(data[0])
     if(data[0]=='take' and data[1]=='off'):
         arm_cmd=CommandBool()
         points=CommandTOL()
 
-        mode_change_client = rospy.Publisher('mavros/set_mode',SetMode, queue_size=1)
-        arming_client = rospy.Publisher('mavros/cmd/arming',CommandBool, queue_size=1)
-        takeoff_client = rospy.Publisher('mavros/cmd/takeoff',CommandTOL, queue_size=1)
+        mod = SetMode()
+        mod.custom_mode = str(52)
+        # mode_change_client = rospy.Service('mavros/set_mode',SetMode)
+        # arming_client = rospy.Service('mavros/cmd/arming',CommandBool)
+        # takeoff_client = rospy.Service('mavros/cmd/takeoff',CommandTOL)
+        mode_change_client = rospy.ServiceProxy('mavros/set_mode',SetMode)
         
-        mode_change_client.publish(mod)
+        mode_change_client(0,'52')
         arm_cmd.request.value = True
         arming_client.call(arm_cmd)
         points.request.altitude = int(data[2],10)
         takeoff_client.publish(points)
 
-    if(data[0]=='points'):
+    if(data[0]=='direction'):
         pos_pub = PoseStamped()
         #setpoint_client = nh.advertise<geometry_msgs::PoseStamped>("mavros/setpoint_position/local",200,true)
         setpoint_client = rospy.Publisher('mavros/setpoint_position/local',PoseStamped, queue_size=10)
