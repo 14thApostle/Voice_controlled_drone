a8f197465041c8e9bd2102a4d356e180d29e3125
Irfaan Arif <irfaan365@gmail.com>
2020-04-21 13:06:12 +0530

    Update Readme and cleanup code

--- README.md
+++ README.md
@@ -1,6 +1,31 @@
 # Voice_controlled_drone
-Can control a drone with simple voice cammands.
-Run speech_recog for voice recognition and publishing the text output to a topic.
-Run speech_drone for subscribing the test output from the previous program and to publish position and velocity setpoints accordingly.
+#### Control a drone using simple voice commands.
+Right now the supported ones are 
+* takeoff
+* direction {x} {y} {z}
+* velocity {x} {y} {z}
+* come
+* go 
+* right 
+* left
+* ascend
+* down
+* spin
 
-For any commits https://github.com/14thApostle/Voice_controlled_drone.git
+Usage 
+----
+```
+roscore
+python3 speech_recog.py
+python3 speech_drone.py
+```
+1) speech_recog for voice recognition and publishes the text output to a topic.
+2) speech_drone for subscribing the test output from the previous program and to publish position and velocity setpoints accordingly.
+
+Dependencies
+---
+``` 
+sudo apt-get install portaudio19-dev    
+sudo apt-get install python-pyaudio python3-pyaudio
+pip3 install SpeechRecognition pyaudio
+```
--- speech_drone.py
+++ speech_drone.py
@@ -38,104 +38,76 @@ def assign(data):
     data = data.split(" ")
     print(data)
 
+    mode_change_client = rospy.ServiceProxy('mavros/set_mode',SetMode)
+    arming_cl = rospy.ServiceProxy("/mavros/cmd/arming", CommandBool)
+    takeoff_client = rospy.ServiceProxy('mavros/cmd/takeoff',CommandTOL)
+
+    setvel_client = rospy.Publisher('/mavros/setpoint_velocity/cmd_vel',TwistStamped, queue_size=1)
+    setpoint_client = rospy.Publisher('mavros/setpoint_position/local',PoseStamped, queue_size=1)
+    setyaw_client = rospy.Publisher('/mavros/global_position/compass_hdg',Float64, queue_size=1)
+    
+    velocity_msg = TwistStamped()
+    pos_pub = current_pos
+    yaw_msg = Float64()
+
+
     if(data[0]=='take' and data[1]=='off'):
-        # Mode Change
-        mode_change_client = rospy.ServiceProxy('mavros/set_mode',SetMode)
+        # Mode Change, Arming, Takeoff
         mode_change_client.call(custom_mode = 'GUIDED')
-
-        # Arming 
-        arming_cl = rospy.ServiceProxy("/mavros/cmd/arming", CommandBool)
         arming_cl.call(True)
-
-        # Takeoff 
-        takeoff_client = rospy.ServiceProxy('mavros/cmd/takeoff',CommandTOL)
         takeoff_client.call(altitude=int(data[2]))
 
     if(data[0]=='direction'):
-
-        # Mavros local position publisher
-        setpoint_client = rospy.Publisher('mavros/setpoint_position/local',PoseStamped, queue_size=1)
-
         d = data[1].split(".")
-        print(d[0])
-        pos_pub = current_pos
         try:
             pos_pub.pose.position.x = float(d[0])
             pos_pub.pose.position.y = float(d[1])
             pos_pub.pose.position.z = float(d[2])
-        except:
-            pass
-
+        except Exception as e:
+            print(e)
         setpoint_client.publish(pos_pub)
 
     if(data[0]=='velocity'):
         # Mavros velocity publisher
-        setvel_client = rospy.Publisher('/mavros/setpoint_velocity/cmd_vel',TwistStamped, queue_size=1)
         d = data[1].split(".")
-        velocity_msg = TwistStamped()
         d.extend(['0','0','0'])
-        print(d)
         velocity_msg.twist.linear.x = float(d[0])
         velocity_msg.twist.linear.y = float(d[1])
         velocity_msg.twist.linear.z = float(d[2])
         setvel_client.publish(velocity_msg)
 
+
     if(data[0]=='come'):
-        # Mavros velocity publisher
-        setvel_client = rospy.Publisher('/mavros/setpoint_velocity/cmd_vel',TwistStamped, queue_size=1)
-        velocity_msg = TwistStamped()
         velocity_msg.twist.linear.x = 4
         setvel_client.publish(velocity_msg)
 
     if(data[0]=='go'):
-        # Mavros velocity publisher
-        setvel_client = rospy.Publisher('/mavros/setpoint_velocity/cmd_vel',TwistStamped, queue_size=1)
-        velocity_msg = TwistStamped()
         velocity_msg.twist.linear.x = -4
         setvel_client.publish(velocity_msg)
 
     if(data[0]=='right'):
-        # Mavros velocity publisher
-        setvel_client = rospy.Publisher('/mavros/setpoint_velocity/cmd_vel',TwistStamped, queue_size=1)
-        velocity_msg = TwistStamped()
         velocity_msg.twist.linear.y = 4
         setvel_client.publish(velocity_msg)
 
     if(data[0]=='left'):
-        # Mavros velocity publisher
-        setvel_client = rospy.Publisher('/mavros/setpoint_velocity/cmd_vel',TwistStamped, queue_size=1)
-        velocity_msg = TwistStamped()
         velocity_msg.twist.linear.y = -4
         setvel_client.publish(velocity_msg)
 
     if(data[0]=='ascend'):
-        # Mavros velocity publisher
-        setvel_client = rospy.Publisher('/mavros/setpoint_velocity/cmd_vel',TwistStamped, queue_size=1)
-        velocity_msg = TwistStamped()
         velocity_msg.twist.linear.z = 4
         setvel_client.publish(velocity_msg)
 
     if(data[0]=='down'):
-        # Mavros velocity publisher
-        setvel_client = rospy.Publisher('/mavros/setpoint_velocity/cmd_vel',TwistStamped, queue_size=1)
-        velocity_msg = TwistStamped()
         velocity_msg.twist.linear.z = -4
         setvel_client.publish(velocity_msg)
 
 
     if(data[0]=='spin'):
-        # Mavros velocity publisher
-        setvel_client = rospy.Publisher('/mavros/setpoint_velocity/cmd_vel',TwistStamped, queue_size=1)
-        velocity_msg = TwistStamped()
         velocity_msg.twist.angular.z = float(4)
         setvel_client.publish(velocity_msg)
 
-        setyaw_client = rospy.Publisher('/mavros/global_position/compass_hdg',Float64, queue_size=1)
-        yaw_msg = Float64()
         yaw_msg.data = compass_hdg
         setyaw_client.publish(yaw_msg)
-
-
 
 print(compass_hdg)
 rospy.spin()
